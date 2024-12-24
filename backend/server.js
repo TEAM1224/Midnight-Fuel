0abc82d6512cbca0972af71f5d57ae17158cfb90
Shubham Singh <132228108+ShubhamSingh1384@users.noreply.github.com>
2024-12-24 17:31:31 +0530

    basic model

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+
+const app = express();
+dotenv.config({path:"./config/config.env"});
+PORT = process.env.PORT || 4000;
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.log("Failed to connect ", error);
+  });
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    method: [ 'GET', 'POST', 'DELETE', 'PUT'],
+    allowedHeaders:[
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma"
+    ],
+    credentials: true
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+
+
+
+
+app.listen(PORT, (err)=>{
+    if(err)
+    console.log(err);
+    else
+    console.log("server started at ", PORT)
+})
